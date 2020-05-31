63404a35a504d9730d399688e6414c39d1f4cfc4
Jad Salhani <salhanijad@gmail.com>
2020-05-31 08:12:03 +0300

    chore: initial commit

---
+++ secure-ios-application/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  secure-ios-application
+//
+//  Created by Salhani, Jad on 5/31/20.
+//  Copyright © 2020 Salhani, Jad. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
